d9d923701e132152fab267aa063821a6ed0e88a6
ArifulIslamAtrafat118 <arafatarifulislam611@gmail.com>
2025-07-29 03:47:25 +0600

    create a hoook for use role based Route Links

---
+++ src/hooks/useRoleBasedLinks.js
@@ -0,0 +1,69 @@
+import { useAuth } from "../context/AuthContext";
+import React from "react";
+import { HiChevronRight } from "react-icons/hi";
+import { MdSpaceDashboard, MdRequestPage } from "react-icons/md";
+import { IoIosCreate } from "react-icons/io";
+import { FaUsers } from "react-icons/fa6";
+import { FaDonate } from "react-icons/fa";
+import useUserData from "../api/useUserData";
+
+function useRoleBasedLinks() {
+  const { currentUser } = useAuth();
+  const { userData } = useUserData();
+  //   const userRole = "admin";
+  const userRole = userData?.role || "donor";
+
+  const donorLinks = [
+    { name: "Dashboard", to: "/dashboard", icon: MdSpaceDashboard },
+    {
+      name: "My Requests",
+      to: "/dashboard/my-donation-requests",
+      icon: MdRequestPage,
+    },
+    {
+      name: "Create Request",
+      to: "/dashboard/create-donation-request",
+      icon: IoIosCreate,
+    },
+  ];
+
+  const adminLinks = [
+    { name: "Dashboard", to: "/dashboard", icon: MdSpaceDashboard },
+    { name: "All Users", to: "/dashboard/all-users", icon: FaUsers },
+    {
+      name: "All Requests",
+      to: "/dashboard/all-blood-donation-request",
+      icon: MdRequestPage,
+    },
+    {
+      name: "Content",
+      to: "/dashboard/content-management",
+      icon: HiChevronRight,
+    },
+    { name: "Funding", to: "/dashboard/funding", icon: FaDonate },
+  ];
+
+  const volunteerLinks = [
+    { name: "Dashboard", to: "/dashboard", icon: MdSpaceDashboard },
+    {
+      name: "All Requests",
+      to: "/dashboard/all-blood-donation-request",
+      icon: MdRequestPage,
+    },
+    {
+      name: "Content",
+      to: "/dashboard/content-management",
+      icon: HiChevronRight,
+    },
+    { name: "Funding", to: "/dashboard/funding", icon: FaDonate },
+  ];
+
+  const roleLinks = {
+    donor: donorLinks,
+    admin: adminLinks,
+    volunteer: volunteerLinks,
+  };
+  return { userRole, roleLinks };
+}
+
+export default useRoleBasedLinks;
