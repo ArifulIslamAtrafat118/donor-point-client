d8ae9a5daba58cacf91654e9603f1ddb30822a81
ArifulIslamAtrafat118 <arafatarifulislam611@gmail.com>
2025-07-29 03:52:44 +0600

    update appRoute: add some routes for under

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -5,6 +5,13 @@ import ErrorPage from "../components/ErrorPage";
 import SignIn from "../pages/Auth/SignIn";
 import SignUp from "../pages/Auth/SignUp";
 import SearchPage from "../pages/SearchPage/SearchPage";
+import BlogPage from "../pages/blogPage/BlogPage";
+import CreateDonationRequest from "../pages/CreateUpdateDonationRequestPage/CreateDonationRequest";
+import PrivateRoute from "./PrivateRoute";
+import DashboardLayout from "../pages/dashboard/DashboardLayout";
+import DashboardHome from "../pages/dashboard/DashboardHome";
+import Profile from "../pages/dashboard/Profile";
+import AllUsers from "../pages/dashboard/allUsers";
 
 export const router = createBrowserRouter([
   {
@@ -27,7 +34,54 @@ export const router = createBrowserRouter([
       {
         path: "/search",
         Component: SearchPage,
-      }
+      },
+      {
+        path: "/blog",
+        Component: BlogPage,
+      },
+    ],
+  },
+  {
+    path: "dashboard",
+    element: (
+      <PrivateRoute>
+        <DashboardLayout />
+      </PrivateRoute>
+    ),
+    children: [
+      {
+        index: true,
+        Component: DashboardHome,
+      },
+      {
+        path: "profile",
+        Component: Profile,
+      },
+      {
+        path: "my-donation-requests",
+        element: <p>my-donation-requests</p>,
+      },
+      {
+        path: "create-donation-request",
+        Component: CreateDonationRequest,
+      },
+      {
+        path: "all-users",
+        element: <AllUsers />,
+      },
+      {
+        path: "all-blood-donation-request",
+        element: <p>all-blood-donation-request</p>,
+      },
+      {
+        path: "content-management",
+        element: <p>content-management</p>,
+      },
+      {
+        path: "funding",
+        element: <p>funding</p>,
+      },
+      
     ],
   },
 ]);
