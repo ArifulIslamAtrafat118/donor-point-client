4eadf51cccc8ad169373106fe8ffb74b79de09e7
ArifulIslamAtrafat118 <arafatarifulislam611@gmail.com>
2025-07-31 02:46:49 +0600

    create hook for fetch allDonationReq, userDonationReq, singleDonationReq data

---
+++ src/api/useDonationRequestsData.js
@@ -0,0 +1,36 @@
+import useAxiosSecure from "../hooks/useAxiosSecure";
+
+const useDonationRequests = () => {
+  const axios = useAxiosSecure();
+
+  const getAllDonationRequests = async ({ page = 1, limit = 10, status = "all" }) => {
+    const res = await axios.get("/donation-request", {
+      params: { page, limit, status },
+    });
+    return res.data; 
+  };
+
+  const getPendingDonationRequests = async () => {
+    const res = await axios.get("/pending-donation-request");
+    return res.data;
+  };
+
+  const getDonationRequestsByUser = async (uid) => {
+    const res = await axios.get(`/donation-request/user/${uid}`);
+    return res.data; 
+  };
+
+  const getDonationRequestById = async (id) => {
+    const res = await axios.get(`/donation-request/${id}`);
+    return res.data; 
+  };
+
+  return {
+    getAllDonationRequests,
+    getPendingDonationRequests,
+    getDonationRequestsByUser,
+    getDonationRequestById,
+  };
+};
+
+export default useDonationRequests;
